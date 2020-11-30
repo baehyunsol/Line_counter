7d8d6b438553c6612e06866189aabc3a53b72dc4
Hyunsol Bae <baehyunsol@naver.com>
2020-11-30 13:15:08 +0000

    prettier output

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import os
 valid = ['.py', '.c', '.md', '.rs', '.cpp', '.h', '.json']
 
 
-def count_lines_and_chars():
+def count():
 
     lines = {}
     chars = {}
+    files = {}
 
     for v in valid:
         lines[v] = 0
         chars[v] = 0
+        files[v] = 0
 
     dirs = os.listdir()
 
@@ -19,14 +21,12 @@ def count_lines_and_chars():
 
         if os.path.isdir(d):
             os.chdir(d)
-            child_lines, child_chars = count_lines_and_chars()
+            child_lines, child_chars, child_files = count()
 
             for v in valid:
-
-                if child_lines[v] > 1:
-                    lines[v] += child_lines[v]
-
+                lines[v] += child_lines[v]
                 chars[v] += child_chars[v]
+                files[v] += child_files[v]
 
             os.chdir('..')
 
@@ -36,8 +36,26 @@ def count_lines_and_chars():
                 ff = f.read()
                 lines[os.path.splitext(d)[1]] += ff.count('\n') + 1
                 chars[os.path.splitext(d)[1]] += len(ff)
+                files[os.path.splitext(d)[1]] += 1
 
-    return lines, chars
+    return lines, chars, files
 
 
-print(count_lines_and_chars())
+def wrapper_count():
+
+    lines, chars, files = count()
+
+    valids = [x for x in files if files[x] > 0]
+    result = {}
+
+    for v in valids:
+        result[v] = {
+            'files': files[v],
+            'lines': lines[v],
+            'chars': chars[v]
+        }
+
+    return result
+
+
+print(wrapper_count())
